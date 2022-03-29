dbc196ecc9a25307cdcd732baad630ffca544f1f
blublin <lublin.ben@gmail.com>
2022-03-29 06:29:01 -0700

    removed  comments, added margiin

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-// let likesArr = [];
-// let likeElements = document.querySelectorAll(".feed-card .feed-likes");
-// for (let elem of likeElements) {
-//     likesArr.push( ParseInt( elem.innerText ) );
-// }
-
 function likeButtonHandler(element) {
     let idValue = element.id;
     let tagLikes = document.querySelector("h3."+idValue)
